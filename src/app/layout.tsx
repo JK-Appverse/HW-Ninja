8a1ec24bb73b02e90384916131ea105280d51e37
Jatin Shakya <jatin1306888@gmail.com>
2025-08-13 09:42:14 +0000

    aur bhai mano app par trrafic ayi to hame kese dikhegi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,23 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* Google Analytics Scripts */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            `,
+          }}
+        />
       </head>
       <body className="font-body antialiased">
         <Providers
